625d332dc69fa2f4090cbc36652878c40c215a91
dinhtanphuc47 <62933963+dinhtanphuc47@users.noreply.github.com>
2020-04-26 23:56:07 +0700

    first commit

---
+++ baitap22/Controllers/SinhVienController.cs
@@ -0,0 +1,90 @@
+﻿using baitap22.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace baitap22.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register1()
+        {
+            return View();
+        }
+        //Request
+        [HttpPost]
+        public ActionResult Register1(String ID, String Name, double Marks)
+        {
+            ID = Request.Form["ID"];
+            Name = Request.Form["Name"];
+            Marks = Convert.ToDouble(Request.Form["Marks"]);
+            ViewData["ID"] = ID;
+            ViewData["Name"] = Name;
+            ViewData["Marks"] = Marks;
+            return View("Index");
+        }
+        //Actions Argument
+        public ActionResult Register2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register2(String ID, String Name, double Marks)
+        {
+            ViewData["ID"] = ID;
+
+            ViewData["Name"] = Name;
+            ViewData["Marks"] = Marks;
+            return View("Index");
+        }
+        //Form Collection
+        public ActionResult Register3()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register3(FormCollection collection)
+        {
+            try
+            {
+                ViewData["ID"] = collection[1];
+                ViewData["Name"] = collection[2];
+                ViewData["Marks"] = collection[3];
+                return View("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //Model
+        public ActionResult Register4()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register4(StudentModel sm)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["ID"] = sm.ID;
+                ViewData["Name"] = sm.Name;
+                ViewData["Marks"] = sm.Marks;
+                return View("index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ baitap22/Models/SinhVienModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace baitap22.Models
+{
+
+    public class StudentModel
+    {
+        [DisplayName("Mã số sinh viên: ")]
+        public String ID { get; set; }
+        [DisplayName("Họ tên: ")]
+        public String Name { get; set; }
+        [DisplayName("Điểm: ")]
+        public double Marks { get; set; }
+    }
+
+}
